1ec5bf32702b34696735565fc51524886d787092
DESKTOP-N920I9F\miks <miksa.csanicz@gmail.com>
2026-05-16 13:19:15 +0200

    jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 const SPEED = 300.0
@@ -6,9 +7,16 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	# Add animations.
+	if velocity.x > 1 or velocity.x < -1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite_2d.animation = "jump"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
